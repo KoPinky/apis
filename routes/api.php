fd880ded89e7f851761d051e5b2fbd0ac195162d
KoPinky <mrlongyst@gmail.com>
2021-01-26 11:21:57 +0700

    asd

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//posts
+// CRUD
 
+// 1. get all(GET)  /api/posts
+// 2. create a post(POST)   /api/posts
+// 3. get a single(GET) /api/posts/{id}
+// 4. update a single (PUT/PATCH)   /api/posts/{id}
+// 5. dalete (delete)   /api/posts/{id}
+
+Route::get('/posts');
+
+
+// to create resurces in laravel
+// 1. create a database and migration
+// 2. create a model
+// 2.5 create a service? Elaquement ORM
+// 3. create a controller to go get info from the database
+// 4. return thet info
+
+Route::get('/test-api', function(){
+    return ['message' => 'hello'];
+});
