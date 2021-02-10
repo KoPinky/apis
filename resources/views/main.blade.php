cd29a7899b9549f34735140ca7bfbafe715b2b5a
KoPinky <mrlongyst@gmail.com>
2021-02-10 15:10:24 +0700

    add stange page

---
+++ resources/views/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+   h1{
+    font-size: 62px;
+}
+
+.center {
+    background: #236542;;
+    width: 62px;
+    height: 62px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+}
+  </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="center">
+    <h1>Выйди от сюда, разбойник!</h1>
+</div>
+    
+</body>
+</html>
